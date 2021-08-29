b3c1f61720e3dfd3c409ca5e6a02327011da5fb0
Jinsoo <aaa119o1125@gamil.com>
2021-08-30 00:24:00 +0900

    좀비 여섯마리 추가

--- FirstTask_0901/js/audio.js
+++ FirstTask_0901/js/audio.js
@@ -23,3 +23,29 @@ btnPause2.onclick = function () {
 btnStop2.onclick = function () {
     myAudio2.pause(); myAudio2.currentTime = 0; // 재생시간을 처음으로 설정 
 } 
+
+// Audio 객체 설정 
+const myAudio3 = new Audio();
+myAudio3.src="https://drive.google.com/uc?export=download&id=1BWDaLqiRRwAyMHGBs9RkELGxq5UTerAW"
+ 
+// 오디오 재생: 크롬브라우저에서 작동 안함
+myAudio3.play(); 
+// 버튼 취득 
+const btnPlay3 = document.getElementById("btnPlay3"); 
+const btnPause3 = document.getElementById("btnPause3");
+const btnStop3 = document.getElementById("btnStop3"); 
+   
+// 재생 버튼 
+btnPlay3.onclick = function () {
+     myAudio3.play();
+}
+   
+// 일시정지 버튼
+btnPause3.onclick = function () { 
+    myAudio3.pause(); 
+}
+    
+// 정지 버튼 
+btnStop3.onclick = function () {
+    myAudio3.pause(); myAudio3.currentTime = 0; // 재생시간을 처음으로 설정 
+} 
